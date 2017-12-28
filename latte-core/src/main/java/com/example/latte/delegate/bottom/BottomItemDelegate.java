f9b9c30b064157e54e00b93f706e6bb675606fc8
Your Name <zhoubk1451@163.com>
2017-12-28 19:06:53 +0800

    微信登录

---
+++ latte-core/src/main/java/com/example/latte/delegate/bottom/BottomItemDelegate.java
@@ -0,0 +1,23 @@
+package com.example.latte.delegate.bottom;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+
+import com.example.latte.delegate.LatteDelegate;
+
+/**
+ * Created by ZBK on 2017-12-28.
+ */
+
+public class BottomItemDelegate extends LatteDelegate{
+    @Override
+    public Object setLayout() {
+        return null;
+    }
+
+    @Override
+    public void onBindView(@Nullable Bundle savedInstanceState, View rootView) {
+
+    }
+}
